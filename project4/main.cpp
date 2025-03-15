98c86d4db4cb0f515be17353b528bdfb474cc81a
ankit-raj78 <ankit.raj78800@gmail.com>
2025-03-15 17:21:39 -0400

    fix: linux vcm chanegs

--- project4/main.cpp
+++ project4/main.cpp
@@ -102,7 +102,7 @@ int main (int argc, char *argv[]) {
     
     try {
         // Establish a connection to the database
-        C = new connection("dbname=ACC_BBALL user=ankitraj2 password=passw0rd hostaddr=127.0.0.1");
+        C = new connection("dbname=ACC_BBALL user=postgres password=passw0rd hostaddr=127.0.0.1");
         if (C->is_open()) {
             cout << "Opened database successfully: " << C->dbname() << endl;
         } else {
